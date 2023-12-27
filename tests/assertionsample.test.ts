642e1116497de5d49c9578a8b8d957cfcc649c3b
Abhishek8188 <abisht123890@gmail.com>
2023-12-27 02:23:13 -0500

    git updated

--- tests/assertionsample.test.ts
+++ tests/assertionsample.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import {loadHomepage, assertTitle} from '../tests/functions';
 
 
-test.only('assertion in heading', async ({ page }) => {
-await loadHomepage()
-await assertTitle()
+test('assertion in heading', async ({ page }) => {
+await loadHomepage(page)
+await assertTitle(page)
 });
--- tests/functions.ts
+++ tests/functions.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-export async function loadHomepage() {
+export async function loadHomepage(page) {
     await page.goto('https:/www.example.com')  
 }
 
-export async function assertTitle() {
+export async function assertTitle(page) {
 await page.waitForSelector('h5')
 const url = await page.url()
 await expect(url).toContain('fail')
